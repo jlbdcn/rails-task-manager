92f9a93a1cce093a6995fdd9684fde33a4d3424d
julien <jlbdcn@gmail.com>
2018-08-15 16:13:03 +0200

    done

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,9 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 #
 # Examples:
+
+Task.create(title: 'Laundry', details: 'Do not mix colors!')
+Task.create(title: 'Studying', details: 'A lot of flashcards to do', completed: true)
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
